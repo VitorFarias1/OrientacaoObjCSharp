382cafc1480b42475f1a26f5473dcdce71293838
Vitor Farias <vitor.rodriguesfarias@gmail.com>
2023-04-05 13:00:09 -0300

    Add exercicios 10 e 11

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int linhas = int.Parse(vet[0]);
+            int colunas = int.Parse(vet[1]);
+
+            int[,] mat = new int[linhas, colunas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < colunas; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            int elemento = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    if (elemento == mat[i, j])
+                    {
+                        Console.WriteLine("Position: " + i + ", " + j + " :");
+
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < colunas - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < linhas - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //O tarcio é linduu
+        }
+    }
+}

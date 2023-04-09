ca0e6310bd1210f358fe6b6f1bb1e84b5fafe6b4
Vitor Farias <vitor.rodriguesfarias@gmail.com>
2023-04-09 16:07:05 -0300

    Criação do Exercício 14 (sobre Herança e polimorfismo)

--- Sobreposicao/Program.cs
+++ Sobreposicao/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Sobreposicao.Entities;
+
+namespace Sobreposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}

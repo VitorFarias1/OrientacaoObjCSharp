1f69ae0d9bb562be308a8259a4ec698eabf81b67
Vitor Farias <vitor.rodriguesfarias@gmail.com>
2023-04-12 15:41:45 -0300

    Exercicio17 criado (Sobre classes e métodos abstratos)

---
+++ Exercicio17/Entities/Contributors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio17.Entities
+{
+    abstract class Contributors
+    {
+        public string Name { get; set; }
+        public double AnnualIncome { get; set; }
+
+        public Contributors()
+        {
+        }
+
+        protected Contributors(string name, double annualIncome)
+        {
+            Name = name;
+            AnnualIncome = annualIncome;
+        }
+
+        public abstract double Imposto();
+    }
+}

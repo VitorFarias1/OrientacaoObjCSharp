f2b1cfc71bfa6fce8c2b803f8446d88093979f66
Vitor Farias <vitor.rodriguesfarias@gmail.com>
2023-04-12 22:32:36 -0300

    Resolvendo merging novamente

---
+++ Exercicio17/Entities/Company.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+
+namespace Exercicio17.Entities
+{
+    internal class Company : Contributors
+    {
+        public int EmployeesNumber { get; set; }
+
+        public Company(int employeesNumber, string name, double annualIncome)
+            : base (name, annualIncome)
+        {
+            EmployeesNumber = employeesNumber;
+        }
+
+        public override double Imposto()
+        {
+            double imposto;
+            if (EmployeesNumber > 10)
+            {
+                imposto = AnnualIncome * 0.14;
+            }
+            else
+            {
+                imposto = AnnualIncome * 0.16;
+            }
+            return imposto;
+        }
+    }
+}
---
+++ Exercicio17/Entities/Individual.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Exercicio17.Entities
+{
+    internal class Individual : Contributors
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(double healthExpenditures, string name, double annualIncome)
+            : base(name,annualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Imposto()
+        {
+            double imposto, saude = 0;
+            if (AnnualIncome < 20000.0)
+            {
+                imposto = AnnualIncome * 0.15;
+            }
+            else
+            {
+                imposto = AnnualIncome * 0.25;
+            }
+            if (HealthExpenditures > 0)
+            {
+                saude = HealthExpenditures * 0.5;
+            }
+            return imposto - saude;
+        }
+    }
+}
